91776d040549310499c80fddd831908e6055f4de
Carlo Munguia <cmunguia@appddictionstudio.com>
2020-04-19 00:51:29 -0500

    added basic user architecture to Rumbl.io

--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -16,6 +16,8 @@ defmodule RumblWeb.Router do
   scope "/", RumblWeb do
     pipe_through :browser
 
+    get "/users", UserController, :index
+    get "/users/:id", UserController, :show
     get "/", PageController, :index
   end
 
