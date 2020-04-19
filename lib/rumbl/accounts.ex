b4ed01e3da6f66ca6a9bc5af26305b1d1d739216
Carlo Munguia <cmunguia@appddictionstudio.com>
2020-04-19 14:41:38 -0500

    rewired app to ecto/postgres & create users table

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -3,26 +3,22 @@ defmodule Rumbl.Accounts do
   The Accounts Context
   """
 
+  alias Rumbl.Repo
   alias Rumbl.Accounts.User
 
-  def list_users do
-    [
-      %User{id: "1", name: "Jose", username: "josevalim"},
-      %User{id: "2", name: "Bruce", username: "redrapids"},
-      %User{id: "3", name: "Chris", username: "chrismccord"}
-    ]
+  def get_user(id) do
+    Repo.get(User, id)
   end
 
-  def get_user(id) do
-    Enum.find(list_users(), fn map -> map.id == id end)
+  def get_user!(id) do
+    Repo.get!(User, id)
   end
 
   def get_user_by(params) do
-    Enum.find(
-      list_users(),
-      fn map ->
-        Enum.all?(params, fn {key, val} ->
-          Map.get(map, key) == val end)
-      end)
+    Repo.get_by(User, params)
+  end
+
+  def list_users do
+    Repo.all(User)
   end
 end
--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,3 +1,11 @@
 defmodule Rumbl.Accounts.User do
-  defstruct [:id, :name, :username]
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
 end
