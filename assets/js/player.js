39741507cbe56ac5a5ff82e8fabff408a25cf6f5
Carlo Munguia <carlomunguia@icloud.com>
2020-09-21 23:06:23 -0500

    created player & video JS files for us to watch our URL links - live! now iterating to get improved association with channels

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,17 +1,15 @@
 let Player = {
     player: null,
-
     init(domId, playerId, onReady) {
         window.onYouTubeIframeAPIReady = () => {
-            this.IframeReady(domId, playerId, onReady)
+            this.onIframeReady(domId, playerId, onReady)
         }
         let youtubeScriptTag = document.createElement("script")
         youtubeScriptTag.src = "//www.youtube.com/iframe_api"
         document.head.appendChild(youtubeScriptTag)
     },
-
     onIframeReady(domId, playerId, onReady) {
-        this.player = new YT.player(domId, {
+        this.player = new YT.Player(domId, {
             height: "360",
             width: "420",
             videoId: playerId,
@@ -26,8 +24,8 @@ let Player = {
     getCurrentTime() {
         return Math.floor(this.player.getCurrentTime() * 1000)
     },
-    seekTo(millSec) {
-        return this.player.seekTo(millSec / 1000)
+    seekTo(millsec) {
+        return this.player.seekTo(millsec / 1000)
     }
 }
 export default Player
